77ebd4f4e36955058e190397e560c41292e3554b
Bubber <bubbertheclayblobofficial@gmail.com>
2026-04-28 17:37:43 -0400

    Update script.js

--- Staticsj/script.js
+++ Staticsj/script.js
@@ -18,7 +18,7 @@ const DEFAULT_SEARCH_ENGINES = {
 let scramjet;
 
 document.addEventListener('DOMContentLoaded', async function () {
-    const basePath = location.pathname.replace(/[^/]*$/, '');
+    const basePath = './';
 
     const { ScramjetController } = $scramjetLoadController();
 
