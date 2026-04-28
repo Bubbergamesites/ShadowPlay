28369f93842fc5255df3e5ea8c48b2101174d6da
Bubber <bubbertheclayblobofficial@gmail.com>
2026-04-28 16:35:44 -0400

    Update sw.js

--- proxy/sw.js
+++ proxy/sw.js
@@ -1,6 +1,6 @@
 // Calculate the dynamic base path for the Service Worker.
 const swPath = self.location.pathname;
-const basePath = swPath.substring(0, swPath.lastIndexOf('/') + 1);
+const basePath = './proxy/';
 
 // Fallback for basePath to ensure it's always defined
 self.basePath = self.basePath || basePath;
